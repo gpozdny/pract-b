0b1ccbddb9d95d0342a555f91c542defead5e69b
Gennady Yudin <soulmatesneverdie92@gmail.com>
2020-04-10 14:53:40 +0400

    desktop

--- js/app.js
+++ js/app.js
@@ -1,70 +1,81 @@
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-	var mySwiper = new Swiper('.swiper-container', {
-		speed: 1000,
-		spaceBetween: 100,
-		centeredSlides: true,
-		navigation: {
-			nextEl: '.fa-caret-right',
-			prevEl: '.fa-caret-left'
-		},
-		autoplay: {
-			delay: 4000,
-		},
-		loop: true,
+	
+// });
 
-		watchOverflow: true,
-		setWrapperSize: true
+var mySwiper = new Swiper('.swiper-container', {
+	speed: 2000,
+	spaceBetween: 100,
+	centeredSlides: true,
+	navigation: {
+		nextEl: '.fa-caret-right',
+		prevEl: '.fa-caret-left'
+	},
+	autoplay: {
+		delay: 4000,
+	},
+	loop: true,
 
-	});
+	watchOverflow: true,
+	setWrapperSize: true
 
-	// POPUP
+});
 
-	let buttonPopUp = document.querySelector('.login__button'),
-		overlay = document.getElementById('overlay'),
-		popUp = document.getElementById('popUp'),
-		crossPopUp = document.getElementById('cross');
+// POPUP
 
-	buttonPopUp.addEventListener('click', function () {
-		showPopUp();
-	});
+let buttonPopUp = document.querySelector('.login__button'),
+	overlay = document.getElementById('overlay'),
+	popUp = document.getElementById('popUp'),
+	crossPopUp = document.getElementById('cross');
 
-	crossPopUp.addEventListener('click', function () {
-		hidePopUp();
-	});
 
-	function showPopUp() {
-		overlay.classList.remove('isHidden'),
-			overlay.classList.add('isShown');
-		popUp.classList.remove('isHidden'),
-			popUp.classList.add('isShown');
-		// document.body.classList.add('scrollIsBlocked');
+	if(buttonPopUp){
+		buttonPopUp.addEventListener('click', function () {
+			showPopUp();
+		});
 	}
 
-	function hidePopUp() {
-		overlay.classList.remove('isShown');
-		popUp.classList.remove('isShown');
-		// document.body.classList.remove('scrollIsBlocked');
+
+crossPopUp.addEventListener('click', function () {
+	hidePopUp();
+});
+
+function showPopUp() {
+	overlay.classList.remove('isHidden'),
+		overlay.classList.add('isShown');
+	popUp.classList.remove('isHidden'),
+		popUp.classList.add('isShown');
+	// document.body.classList.add('scrollIsBlocked');
+}
+
+function hidePopUp() {
+	overlay.classList.remove('isShown');
+	popUp.classList.remove('isShown');
+	// document.body.classList.remove('scrollIsBlocked');
+}
+
+// Check student ID
+
+let form = document.getElementById('submit'),
+	inputValue = document.getElementById('value');
+
+form.addEventListener('click', function () {
+	let studentID = inputValue.value;
+
+	switch (studentID) {
+		case '1nna':
+			alert(`RIGHT!`);
+			break;
+		case 'soph1e':
+			alert(`RIGHT!`);
+			break;
+		default:
+			alert(`there is no such ID`);
+			event.preventDefault();
 	}
+});
 
-	// Check student ID
-
-	let form = document.getElementById('submit'),
-		inputValue = document.getElementById('value');
-
-	form.addEventListener('click', function () {
-		let studentID = inputValue.value;
-
-		switch (studentID) {
-			case '1nna':
-				alert(`RIGHT!`);
-				break;
-			case 'soph1e':
-				alert(`RIGHT!`);
-				break;
-			default:
-				alert(`there is no such ID`);
-				event.preventDefault();
-		}
-	});
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'js/particles.json', function () {
+	console.log('callback - particles.js config loaded');
 });
